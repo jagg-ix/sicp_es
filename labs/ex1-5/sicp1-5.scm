2cde6ef458a06f91dbf8e29bdc592aba49f95250
Jorge Garcia <jorge.garcia.gonzalez@gmail.com>
2016-09-21 18:23:12 -0500

    Add functions from Ben Bitdiddle

--- labs/ex1-5/sicp1-5.scm
+++ labs/ex1-5/sicp1-5.scm
@@ -1,3 +1,17 @@
-(define (solve-it )
 
-)
+
+; Defined by Ben Bitdiddle
+
+(define (p) (p))
+
+(define (test x y)
+ (if (= x 0)
+     0
+     y
+     ))
+
+
+(define (solve-it )
+   (test 0 (p))
+   )
+
