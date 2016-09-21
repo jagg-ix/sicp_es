ab5a23bf49e33184504a618776d83da1a61c5aee
Jorge Garcia <jorge.garcia.gonzalez@gmail.com>
2016-09-21 01:06:34 -0500

    solution to exercise 1.4

--- labs/ex1-4/Readme.md
+++ labs/ex1-4/Readme.md
@@ -6,3 +6,41 @@
 (define  (a-plus-abs-b a b)
   ( (if (> b 0) + -) a b))
              
+
+
+## Solution once I run them under mit-scheme: 
+```scheme 
+  (a-plus-abs-b (+ 2 2) 10) ; evaluate 2+2 first, call (a-plus-abs-b 4 10)
+  (a-plus-abs-b (- 2 2) 10) ; evaluate 2-2 first, call (a-plus-abs-b 0 10)
+  (a-plus-abs-b (* 2 2) 10) ; evaluate 2*2 first, call (a-plus-abs-b 4 10)
+  (a-plus-abs-b (/ 2 2) 10) ; evaluate 2*2 first, call (a-plus-abs-b 1 10)
+
+  (a-plus-abs-b 10 (+ 2 2) ) ; evaluate 2+2 first, call (a-plus-abs-b 10 4 )
+  (a-plus-abs-b 10 (- 2 2) ) ; evaluate 2-2 first, call (a-plus-abs-b 10 0 )
+  (a-plus-abs-b 10 (* 2 2) ) ; evaluate 2*2 first, call (a-plus-abs-b 10 4 )
+  (a-plus-abs-b 10 (/ 2 2) ) ; evaluate 2*2 first, call (a-plus-abs-b 10 1 )
+
+;Value: 14
+
+1 ]=> 
+;Value: 10
+
+1 ]=> 
+;Value: 14
+
+1 ]=> 
+;Value: 11
+
+1 ]=> 
+;Value: 14
+
+1 ]=> 
+;Value: 10
+
+1 ]=> 
+;Value: 14
+
+1 ]=> 
+;Value: 11
+
+1 ]=> 
--- labs/ex1-4/sicp1-4.scm
+++ labs/ex1-4/sicp1-4.scm
@@ -1,7 +1,18 @@
+; Function returns a + b when b is bigger
+; Function retuns  a - b otherwise
 (define  (a-plus-abs-b a b)
-  ( (if (> b 0) + -) a b))
+  ( (if (> b 0) + -) a b) )
 
 ; With evaluation model 
-(define (sicp1-4 a b)
-  (a-plus-abs-b a b)
+; it means parameters are evaluated, value evaluated is pass it as parameter
+(define (solve-it )
+  (a-plus-abs-b (+ 2 2) 10) ; evaluate 2+2 first, call (a-plus-abs-b 4 10)
+  (a-plus-abs-b (- 2 2) 10) ; evaluate 2-2 first, call (a-plus-abs-b 0 10)
+  (a-plus-abs-b (* 2 2) 10) ; evaluate 2*2 first, call (a-plus-abs-b 4 10)
+  (a-plus-abs-b (/ 2 2) 10) ; evaluate 2*2 first, call (a-plus-abs-b 1 10)
+
+  (a-plus-abs-b 10 (+ 2 2) ) ; evaluate 2+2 first, call (a-plus-abs-b 10 4 )
+  (a-plus-abs-b 10 (- 2 2) ) ; evaluate 2-2 first, call (a-plus-abs-b 10 0 )
+  (a-plus-abs-b 10 (* 2 2) ) ; evaluate 2*2 first, call (a-plus-abs-b 10 4 )
+  (a-plus-abs-b 10 (/ 2 2) ) ; evaluate 2*2 first, call (a-plus-abs-b 10 1 )
 ) 
